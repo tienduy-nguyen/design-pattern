0f14eb2702373ad7f2a2fa9c5fad90f860e8e055
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-12 11:52:34 +0100

    Update more example for observer pattern

---
+++ design-pattern-typescript/behavior/observer-pattern/observer3.ts
@@ -0,0 +1,77 @@
+export class Subject {
+  private observers: Observer[] = [];
+
+  public register(observer: Observer): void {
+    console.log(observer, 'is pushed!');
+    this.observers.push(observer);
+  }
+
+  public unregister(observer: Observer): void {
+    var n: number = this.observers.indexOf(observer);
+    console.log(observer, 'is removed');
+    this.observers.splice(n, 1);
+  }
+
+  public notify(): void {
+    console.log('notify all the observers', this.observers);
+    var i: number, max: number;
+    for (i = 0, max = this.observers.length; i < max; i += 1) {
+      this.observers[i].notify();
+    }
+  }
+}
+
+export class ConcreteSubject extends Subject {
+  private subjectState: number;
+
+  get SubjectState(): number {
+    return this.subjectState;
+  }
+
+  set SubjectState(subjectState: number) {
+    this.subjectState = subjectState;
+  }
+}
+
+export class Observer {
+  public notify(): void {
+    throw new Error('Abstract Method!');
+  }
+}
+
+export class ConcreteObserver extends Observer {
+  private name: string;
+  private state: number;
+  private subject: ConcreteSubject;
+
+  constructor(subject: ConcreteSubject, name: string) {
+    super();
+    console.log('ConcreteObserver', name, 'is created!');
+    this.subject = subject;
+    this.name = name;
+  }
+
+  public notify(): void {
+    console.log("ConcreteObserver's notify method");
+    console.log(this.name, this.state);
+    this.state = this.subject.SubjectState;
+  }
+
+  get Subject(): ConcreteSubject {
+    return this.subject;
+  }
+
+  set Subject(subject: ConcreteSubject) {
+    this.subject = subject;
+  }
+}
+
+/**
+ * This pattern suggests, that you have a subject and some observers.
+ * Every time you update your subject state, observers get notified about it.
+ * This pattern is very handy when you need to tie several objects
+ * to each other with abstraction and freedom of implementation.
+ * Also, this pattern is a key part of the familiar model-view-controller (MVC)
+ * architectural pattern.
+ * Strongly used in almost every GUI library.
+ */
